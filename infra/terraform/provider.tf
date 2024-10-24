8bd5c1177933aade10cd0bfb9da0eb4c00419b14
David Vardanian <davardanian@gmail.com>
2024-10-24 16:51:02 +0400

    include tfc backend setup

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "6.8.0"
     }
   }
+   backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "Denver"
+
+    workspaces {
+      name = "gcp-random-stuff"
+    }
+  }
 }
 
 provider "google" {
